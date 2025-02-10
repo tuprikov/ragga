4cc9277593b20c7ca509e769c0f42240fa99cf0e
tuprikov <tuprikov@gmail.com>
2025-02-10 22:02:32 +0100

    Remove unused imports from the web scraping module

--- scraper.py
+++ scraper.py
@@ -1,12 +1,9 @@
 """
 Web scraping module.
 """
-import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from urllib.parse import urljoin, urlparse
 from webdriver_manager.chrome import ChromeDriverManager
